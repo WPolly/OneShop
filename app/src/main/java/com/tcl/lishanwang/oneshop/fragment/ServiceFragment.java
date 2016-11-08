97125a35e9adde2c4ed25b6a73cc62c04153f5e6
lishan <lishan.wang@tcl.com>
2016-11-08 15:04:51 +0800

    1, Add toolbar on LoginActiviy. 2, Add ButterKnife library.

--- app/src/main/java/com/tcl/lishanwang/oneshop/fragment/ServiceFragment.java
+++ app/src/main/java/com/tcl/lishanwang/oneshop/fragment/ServiceFragment.java
@@ -1,14 +1,23 @@
 package com.tcl.lishanwang.oneshop.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tcl.lishanwang.oneshop.R;
+import com.tcl.lishanwang.oneshop.activity.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by lishan on 2016/11/4.
@@ -17,6 +26,36 @@ import com.tcl.lishanwang.oneshop.R;
 
 public class ServiceFragment extends Fragment {
 
+    @BindView(R.id.civ_user_profile)
+    CircleImageView mCivUserProfile;
+    @BindView(R.id.ll_user_profile_container)
+    LinearLayout mLlUserProfileContainer;
+    @BindView(R.id.tv_all_orders)
+    TextView mTvAllOrders;
+    @BindView(R.id.ll_to_pay)
+    LinearLayout mLlToPay;
+    @BindView(R.id.imageView)
+    ImageView mImageView;
+    @BindView(R.id.ll_to_receive)
+    LinearLayout mLlToReceive;
+    @BindView(R.id.ll_to_comment)
+    LinearLayout mLlToComment;
+    @BindView(R.id.ll_returns)
+    LinearLayout mLlReturns;
+    @BindView(R.id.ll_favorite)
+    LinearLayout mLlFavorite;
+    @BindView(R.id.ll_coupons)
+    LinearLayout mLlCoupons;
+    @BindView(R.id.ll_others)
+    LinearLayout mLlOthers;
+    @BindView(R.id.ll_sevice_site)
+    LinearLayout mLlSeviceSite;
+    @BindView(R.id.ll_usual_question)
+    LinearLayout mLlUsualQuestion;
+    @BindView(R.id.ll_contact_us)
+    LinearLayout mLlContactUs;
+    @BindView(R.id.ll_feedback)
+    LinearLayout mLlFeedback;
 
     public static ServiceFragment newInstance(int someInt) {
         ServiceFragment myFragment = new ServiceFragment();
@@ -36,11 +75,51 @@ public class ServiceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return LayoutInflater.from(getContext()).inflate(R.layout.fragment_service, container, false);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_service, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @OnClick({R.id.civ_user_profile, R.id.ll_user_profile_container, R.id.tv_all_orders, R.id.ll_to_pay, R.id.imageView, R.id.ll_to_receive, R.id.ll_to_comment, R.id.ll_returns, R.id.ll_favorite, R.id.ll_coupons, R.id.ll_others, R.id.ll_sevice_site, R.id.ll_usual_question, R.id.ll_contact_us, R.id.ll_feedback})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.civ_user_profile:
+                break;
+            case R.id.ll_user_profile_container:
+                Intent intent  = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tv_all_orders:
+                break;
+            case R.id.ll_to_pay:
+                break;
+            case R.id.imageView:
+                break;
+            case R.id.ll_to_receive:
+                break;
+            case R.id.ll_to_comment:
+                break;
+            case R.id.ll_returns:
+                break;
+            case R.id.ll_favorite:
+                break;
+            case R.id.ll_coupons:
+                break;
+            case R.id.ll_others:
+                break;
+            case R.id.ll_sevice_site:
+                break;
+            case R.id.ll_usual_question:
+                break;
+            case R.id.ll_contact_us:
+                break;
+            case R.id.ll_feedback:
+                break;
+        }
     }
 }
