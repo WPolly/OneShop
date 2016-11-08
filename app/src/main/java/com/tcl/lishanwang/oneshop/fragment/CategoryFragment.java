be095fad16727b8f5b891e5dc309a3b56b5ba94b
lishan <lishan.wang@tcl.com>
2016-11-08 11:25:40 +0800

    Refactor category fragment, discards recycler view, using radio group instead.

--- app/src/main/java/com/tcl/lishanwang/oneshop/fragment/CategoryFragment.java
+++ app/src/main/java/com/tcl/lishanwang/oneshop/fragment/CategoryFragment.java
@@ -3,29 +3,19 @@ package com.tcl.lishanwang.oneshop.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 import com.tcl.lishanwang.oneshop.R;
-import com.tcl.lishanwang.oneshop.adapter.CategoryLeftAdapter;
-import com.tcl.lishanwang.oneshop.bean.CategoryMenuItemBean;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Created by lishan on 2016/11/7.
- * 13:54
+ * Created by lishan on 2016/11/8.
+ * 10:52
  */
 
 public class CategoryFragment extends Fragment {
-
-
-    private RecyclerView mRvCategoryLeft;
-    private List<CategoryMenuItemBean> mCategoryMenuItemBeen;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,19 +26,13 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_category, container, false);
-        mRvCategoryLeft = (RecyclerView) rootView.findViewById(R.id.rv_category_left);
+        ViewGroup vgCategoryLeft = (ViewGroup) rootView.findViewById(R.id.rg_category_left);
+        ((RadioButton) vgCategoryLeft.getChildAt(0)).setChecked(true);
         return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mCategoryMenuItemBeen = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            CategoryMenuItemBean bean = new CategoryMenuItemBean();
-            mCategoryMenuItemBeen.add(bean);
-        }
-        mRvCategoryLeft.setAdapter(new CategoryLeftAdapter(mCategoryMenuItemBeen));
-        mRvCategoryLeft.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
