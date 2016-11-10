d894cb6b7463bec06e19b5ee4204b1589db58b1f
lishan <lishan.wang@tcl.com>
2016-11-10 14:33:25 +0800

    Add looping img on HomeFragment with sample imgs.

---
+++ app/src/main/java/com/tcl/lishanwang/oneshop/adapter/HomeLoopingImgViewPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.tcl.lishanwang.oneshop.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.tcl.lishanwang.oneshop.R;
+
+/**
+ * Created by lishan on 2016/11/10.
+ * 13:53
+ */
+
+public class HomeLoopingImgViewPagerAdapter extends PagerAdapter {
+
+    private int[] imgIds =
+            {R.mipmap.discovery_hot_topic, R.mipmap.discovery_hot_activity, R.mipmap.discovery_sweepstakes, R.mipmap.discovery_physical_store};
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(container.getContext());
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(imgIds[position]);
+        container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return imgIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/tcl/lishanwang/oneshop/fragment/HomeFragment.java
+++ app/src/main/java/com/tcl/lishanwang/oneshop/fragment/HomeFragment.java
@@ -3,11 +3,13 @@ package com.tcl.lishanwang.oneshop.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tcl.lishanwang.oneshop.R;
+import com.tcl.lishanwang.oneshop.adapter.HomeLoopingImgViewPagerAdapter;
 
 /**
  * Created by lishan on 2016/11/4.
@@ -35,7 +37,10 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewPager vpHomeLoopingImg = (ViewPager) rootView.findViewById(R.id.vp_home_looping_img);
+        vpHomeLoopingImg.setAdapter(new HomeLoopingImgViewPagerAdapter());
+        return rootView;
     }
 
     @Override
