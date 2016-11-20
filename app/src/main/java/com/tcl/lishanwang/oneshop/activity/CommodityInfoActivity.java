9df4432feb0598fbda815d1b7e2114f62c693425
lishan <643633832@qq.com>
2016-11-20 21:10:35 +0800

    Add TabLayout associated with a sample ViewPager on CommodityInfoActivity.

--- app/src/main/java/com/tcl/lishanwang/oneshop/activity/CommodityInfoActivity.java
+++ app/src/main/java/com/tcl/lishanwang/oneshop/activity/CommodityInfoActivity.java
@@ -1,15 +1,25 @@
 package com.tcl.lishanwang.oneshop.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.tcl.lishanwang.oneshop.R;
+import com.tcl.lishanwang.oneshop.adapter.CommodityInfoViewPagerAdapter;
 
 public class CommodityInfoActivity extends AppCompatActivity {
+
+    private TabLayout mTlCommodityInfo;
+    private ViewPager mVpCommodityInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commodity_info);
+        mTlCommodityInfo = (TabLayout) findViewById(R.id.tl_commodity_info);
+        mVpCommodityInfo = (ViewPager) findViewById(R.id.vp_commodity_info);
+        mVpCommodityInfo.setAdapter(new CommodityInfoViewPagerAdapter(getSupportFragmentManager()));
+        mTlCommodityInfo.setupWithViewPager(mVpCommodityInfo);
     }
 }
---
+++ app/src/main/java/com/tcl/lishanwang/oneshop/adapter/CommodityInfoViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.tcl.lishanwang.oneshop.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.tcl.lishanwang.oneshop.fragment.ServiceFragment;
+
+/**
+ * Created by xiaoshan on 2016/11/20.
+ * 20:01
+ */
+
+public class CommodityInfoViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private String[] titles = {"Goods", "Detail", "Parameter", "Comments"};
+
+    public CommodityInfoViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new ServiceFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
